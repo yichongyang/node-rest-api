ac5312f694b6f469ba7c7861e3a53dc3b0022a4b
yichongyang <33671891+yichongyang@users.noreply.github.com>
2021-12-25 18:08:27 -0500

    Map both http and https ports

--- startNodeApps.bat
+++ startNodeApps.bat
@@ -1,5 +1,5 @@
 @echo off
 
-start /B docker run -p 8443:8443 --name yyang-node-app yyang/node-app > nul 2>&1
+start /B docker run -p 8443:8443 -p 8080:8080 --name yyang-node-app yyang/node-app > nul 2>&1
 
 exit 0
