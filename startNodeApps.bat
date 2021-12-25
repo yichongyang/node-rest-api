c9288602bc4eb36aa4c51ced3a763c5bca798bcd
yichongyang <33671891+yichongyang@users.noreply.github.com>
2021-12-25 18:25:48 -0500

    Remove http port mapping

--- startNodeApps.bat
+++ startNodeApps.bat
@@ -1,5 +1,5 @@
 @echo off
 
-start /B docker run -p 8443:8443 -p 8000:8000 --name yyang-node-app yyang/node-app > nul 2>&1
+start /B docker run -p 8443:8443 --name yyang-node-app yyang/node-app > nul 2>&1
 
 exit 0
