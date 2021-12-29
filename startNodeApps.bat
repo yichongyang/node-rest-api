39b4f8ad3bea95a30a36936e4818a2c35d1b2443
yichongyang <33671891+yichongyang@users.noreply.github.com>
2021-12-29 11:16:52 -0500

    Remove exit 0 at the end

--- startNodeApps.bat
+++ startNodeApps.bat
@@ -1,5 +1,3 @@
 @echo off
 
-start /B docker run -p 8443:8443 --name yyang-node-app yyang/node-app > nul 2>&1
-
-exit 0
+start /B docker run -d -p 8443:8443 --name yyang-node-app yyang/node-app > nul 2>&1
